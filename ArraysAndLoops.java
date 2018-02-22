22e01ec38d988b4d4837c6435ccc77ecd7951f30
treyshel <tshelton@basecampcodingacademy.org>
2018-02-22 11:53:31 -0600

    i've created a few more method examples since my last commit.. one more example for arrays and loops practice and then on to objects

--- ArraysAndLoops.java
+++ ArraysAndLoops.java
@@ -1,17 +1,19 @@
 package com.example.starting_spring;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class ArraysAndLoops {
     @PostMapping ("/lastThree")
-    public List<String> lastThree(@RequestBody List<String> l) {
+    public List lastThree(@RequestBody List l) {
         return l.subList(l.size() -3, l.size());
     }
     @PostMapping ("/sumOFlist")
@@ -22,19 +24,45 @@ public class ArraysAndLoops {
         }
         return sum;
     }
-    @PostMapping ("/sumOFlonger")
-    public List<Double> sumOFlonger(@RequestBody List<Double> l1, @RequestBody List<Double> l2) {
+    @GetMapping ("/sumOFlonger")
+    public double sumOFlonger(@RequestParam List<Double> l1, @RequestParam List<Double> l2) {
         if (l1.size() > l2.size()) {
-            return l1;
-        }
-        return l2;
+            return sumOFlist(l1);
+        } else if (l1.size() < l2.size()){
+            return sumOFlist(l2);
+        } else
+            return sumOFlist(l1) + sumOFlist(l2);
     }
-    @PostMapping ("/differenceFromMinimum")
-    public List<Double> differenceFromMinimum(@RequestBody List<Double> l) {
-        List new_l = new ArrayList();
+    @GetMapping ("/differenceFromMinimum")
+    public ArrayList<Integer> differenceFromMinimum(@RequestParam ArrayList<Integer> l) {
+        int minValue = Collections.min(l);
+
         for (int i = 0; i <  l.size(); i++) {
-            new_l.add(2);
+            int fixedL = (l.get(i) - minValue);
+            l.set(i, fixedL);
+    }
+    return l;
+    }
+    @GetMapping ("/hashtags")
+    public String hashtagsInPost(@RequestParam String tweet) {
+        String[] hashtag = tweet.split(" ");
+        String result = "";
+        for (int x = 0; x < hashtag.length; x++) {
+            if (hashtag[x].startsWith("#")) {
+                result += " " + hashtag[x];
+            }
         }
-        return new_l;
+        return result.trim();
+    }
+    @GetMapping ("/mentions")
+    public String mentionedInPost(@RequestParam String tweet) {
+        String[] mentionedAt = tweet.split(" ");
+        String result = "";
+        for (int x = 0; x < mentionedAt.length; x++) {
+            if (mentionedAt[x].startsWith("@")) {
+                result += " " + mentionedAt[x];
+            }
+        }
+        return result.trim();
     }
 }
